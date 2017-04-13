ce705cb416b745fde92c384c530fae46ad6c5f1a
Chandraprakash Dwivedi <chandra.prakash363@gmail.com>
2017-04-13 16:00:03 +0530

    Update Incremental_sql_backup.sh

--- Incremental_sql_backup.sh
+++ Incremental_sql_backup.sh
@@ -31,3 +31,8 @@ done
 
 lftp -u 'ftpuser,ftppassword' backupspace.rimuhosting.com -e "set ftp:ssl-protect-data true; mirror -er --reverse -I *.bz2 -X $newestlog /var/lib/mysql /myvar/lib/mysql; mput /var/lib/mysql/mybinlog.index -O /myvar/lib/mysql; exit;"
 #echo "Bin Logs backed up"
+#Edit the above script to reflect your mysql username (probably root) and password, as well as your FTP backup server username and password.  Replace "database1 database2" with the names of the databases to back up. (The "echoed" statements will be emailed to you, the system administrator, by cron.  Remove these lines if you do not want the emails.)
+#The script handles both incremental and full backups, compresses all files before transmission, and automatically cleans outdated backup files from the backup ftp server.  It performs one full backup per week, on Sunday at 06:02.  You can force the creation of a full backup (for testing or whatever purpose) by passing the argument "dump".
+#The "flush-logs" mysql command is used to create a new binary log file.  This command might be issued to the mysql server outside of this script (such as when the mysql server is restarted), producing an extra binary log file.  In this case, the script will compress and back up the extra binary log file the next time backup is done.
+#Store the following line in /etc/crontab to execute newmysqlbinlog every 6 hours (this is of course configurable):
+#2 */6 * * * root /root/crontasks/newmysqlbinlog
